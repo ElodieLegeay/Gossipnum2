918ac92018044da33a64ab0b9441bbc97c986230
ElodieLegeay <legeayelodie@gmail.com>
2018-02-14 11:15:28 +0100

    init

---
+++ app/models/gossip.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: gossips
+#
+#  id               :integer          not null, primary key
+#  anonymous_author :string
+#  content          :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
+class Gossip < ApplicationRecord
+	belongs_to :user, :optional => true
+end
